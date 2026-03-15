098104ee9420b5e81f7fe4ce58a0001c8a21e473
0xJotave <joaovmb15@gmail.com>
2026-03-15 05:06:01 -0300

    feat: create exceptions

--- CoreService/src/main/java/com/eventmanager/coreservice/domain/exception/EventNotFound.java
+++ CoreService/src/main/java/com/eventmanager/coreservice/domain/exception/EventNotFound.java
@@ -2,6 +2,6 @@ package com.eventmanager.coreservice.domain.exception;
 
 public class EventNotFound extends RuntimeException {
     public EventNotFound(String message) {
-        super("[ERROR] " + message);
+        super(message);
     }
 }
